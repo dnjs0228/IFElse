7203d4fa3e2d50cece99bd13026daa9956293705
dnjs0228 <dnjsrud0228@naver.com>
2014-10-02 16:38:11 +0900

    실습 5

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup()
 }
 
   function draw()
-  
+
 {
   background(0);
  // draw function
@@ -22,5 +22,61 @@ if (mouseX < 100) {
   ellipse(450, 200, 100, 100);
 } else {
   ellipse(550, 200, 100, 100);
+}
+// draw function
+if (mouseX < 100) {
+  ellipse(50, 400, 100, 100);
+} else if (mouseX < 200){
+  ellipse(150, 400, 100, 100);
+} else if (mouseX < 300){
+  ellipse(250, 400, 100, 100);
+} else if (mouseX < 400){
+  ellipse(350, 400, 100, 100);
+} else if (mouseX < 500){
+  ellipse(450, 400, 100, 100);
+} else {
+  ellipse(550, 400, 100, 100);
+}
+// draw function
+if (mouseX < 100) {
+  ellipse(100, 300, 200, 100);
+} else if (mouseX < 200){
+  ellipse(200, 300, 200, 100);
+} else if (mouseX < 300){
+  ellipse(300, 300, 200, 100);
+} else if (mouseX < 400){
+  ellipse(400, 300, 200, 100);
+} else if (mouseX < 500){
+  ellipse(500, 300, 200, 100);
+} else {
+  ellipse(600, 300, 200, 100);
+}
+// draw function
+if (mouseX < 100) {
+  ellipse(100, 500, 200, 100);
+} else if (mouseX < 200){
+  ellipse(200, 500, 200, 100);
+} else if (mouseX < 300){
+  ellipse(300, 500, 200, 100);
+} else if (mouseX < 400){
+  ellipse(400, 500, 200, 100);
+} else if (mouseX < 500){
+  ellipse(500, 500, 200, 100);
+} else {
+  ellipse(600, 500, 200, 100);
+}
+// draw function
+if (mouseX < 100) {
+  ellipse(100, 100, 200, 100);
+} else if (mouseX < 200){
+  ellipse(200, 100, 200, 100);
+} else if (mouseX < 300){
+  ellipse(300, 100, 200, 100);
+} else if (mouseX < 400){
+  ellipse(400, 100, 200, 100);
+} else if (mouseX < 500){
+  ellipse(500, 100, 200, 100);
+} else {
+  ellipse(600, 100, 200, 100);
 }
 }
