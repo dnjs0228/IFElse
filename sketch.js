3b56c2fb226e367537f36444953b20f9453a6f40
dnjs0228 <dnjsrud0228@naver.com>
2014-10-02 16:42:41 +0900

    실습 8

--- sketch.js
+++ sketch.js
@@ -67,16 +67,16 @@ if (mouseX < 100) {
 
 // draw function
 if (mouseX < 100) {
-  ellipse(100, 100, 200, 100);
+  ellipse(100, 100, 100, 100);
 } else if (mouseX < 200){
-  ellipse(200, 100, 200, 100);
+  ellipse(200, 100, 100, 100);
 } else if (mouseX < 300){
-  ellipse(300, 100, 200, 100);
+  ellipse(300, 100, 100, 100);
 } else if (mouseX < 400){
-  ellipse(400, 100, 200, 100);
+  ellipse(400, 100, 100, 100);
 } else if (mouseX < 500){
-  ellipse(500, 100, 200, 100);
+  ellipse(500, 100, 100, 100);
 } else {
-  ellipse(600, 100, 200, 100);
+  ellipse(600, 100, 100, 100);
 }
 }
