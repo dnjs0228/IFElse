95ef214d32e38ff189e006ce49784ece2202747f
dnjs0228 <dnjsrud0228@naver.com>
2014-10-02 16:18:28 +0900

    실습 2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-function setup() {
-  createCanvas(640, 480);
-  background(0);
-  stroke(255);
+function setup()
+ {
+  createCanvas(800, 600);
   noFill();
+  stroke(255, 0, 0);
+}
 
-  line(10, 10, 90, 10); // x1, y1, x2, y2
-    rect(20, 20, 80, 50); // x, y, width, height
-    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+  function draw()
+{
+  background(0);
+  if (mouseX < 300) {
+  ellipse(200,200,100,100);
+}
 }
